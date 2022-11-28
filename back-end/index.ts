0471675d5ad393b627a115899f22f9052211207b
da.valencia <da.valencia@globant.com>
2022-11-28 00:13:36 -0500

    WIP login

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,13 +1,17 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app: Express = express();
-const port = 8000;
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.post('/login', (req: Request, res: Response) => {
+  res.json({
+    email:'test@test.com',
+    password:'Test1234'
+  });
 });
 
 app.listen(port, () => {
